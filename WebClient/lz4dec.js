398fad4b6ceaaf64dda3020e159fa0756f1422ee
pdaxrom <sashz@pdaxrom.org>
2021-04-27 13:45:05 +0400

    lz4 block decoder

--- WebClient/lz4dec.js
+++ WebClient/lz4dec.js
@@ -1,70 +1,17 @@
+//
+// https://github.com/Benzinga/lz4js
+//
 // lz4.js - An implementation of Lz4 in plain JavaScript.
 //
-// TODO:
-// - Unify header parsing/writing.
-// - Support options (block size, checksums)
-// - Support streams
-// - Better error handling (handle bad offset, etc.)
-// - HC support (better search algorithm)
-// - Tests/benchmarking
 
-function Util() {
-// Simple hash function, from: http://burtleburtle.net/bob/hash/integer.html.
-// Chosen because it doesn't use multiply and achieves full avalanche.
-    this.hashU32 = function hashU32(a) {
-	a = a | 0;
-	a = a + 2127912214 + (a << 12) | 0;
-	a = a ^ -949894596 ^ a >>> 19;
-	a = a + 374761393 + (a << 5) | 0;
-	a = a + -744332180 ^ a << 9;
-	a = a + -42973499 + (a << 3) | 0;
-	return a ^ -1252372727 ^ a >>> 16 | 0;
-    }
+function LZ4() {
 
-// Reads a 64-bit little-endian integer from an array.
-    this.readU64 = function readU64(b, n) {
-	var x = 0;
-	x |= b[n++] << 0;
-	x |= b[n++] << 8;
-	x |= b[n++] << 16;
-	x |= b[n++] << 24;
-	x |= b[n++] << 32;
-	x |= b[n++] << 40;
-	x |= b[n++] << 48;
-	x |= b[n++] << 56;
-	return x;
-    }
+    // Compression format parameters/constants.
+    var minMatch = 4;
 
-// Reads a 32-bit little-endian integer from an array.
-    this.readU32 = function readU32(b, n) {
-	var x = 0;
-	x |= b[n++] << 0;
-	x |= b[n++] << 8;
-	x |= b[n++] << 16;
-	x |= b[n++] << 24;
-	return x;
-    }
+    // Utility functions/primitives
+    // --
 
-// Writes a 32-bit little-endian integer from an array.
-    this.writeU32 = function writeU32(b, n, x) {
-	b[n++] = (x >> 0) & 0xff;
-	b[n++] = (x >> 8) & 0xff;
-	b[n++] = (x >> 16) & 0xff;
-	b[n++] = (x >> 24) & 0xff;
-    }
-
-// Multiplies two numbers using 32-bit integer multiplication.
-// Algorithm from Emscripten.
-    this.imul = function imul(a, b) {
-	var ah = a >>> 16;
-	var al = a & 65535;
-	var bh = b >>> 16;
-	var bl = b & 65535;
-
-	return al * bl + (ah * bl + al * bh << 16) | 0;
-    }
-
-    // Makes a byte buffer. On older browsers, may return a plain array.
     this.makeBuffer = function makeBuffer (size) {
 	try {
 	    return new Uint8Array(size);
@@ -78,45 +25,6 @@ function Util() {
 	    return buf;
 	}
     }
-}
-
-function LZ4() {
-
-    var util = new Util();
-
-    // Constants
-    // --
-
-    // Compression format parameters/constants.
-    var minMatch = 4;
-
-    // Shared buffers
-    var blockBuf = util.makeBuffer(5 << 20);
-
-    // Frame constants.
-    var magicNum = 0x184D2204;
-
-    // Frame descriptor flags.
-    var fdContentChksum = 0x4;
-    var fdContentSize = 0x8;
-    var fdBlockChksum = 0x10;
-    // var fdBlockIndep = 0x20;
-    var fdVersion = 0x40;
-    var fdVersionMask = 0xC0;
-
-    // Block sizes.
-    var bsUncompressed = 0x80000000;
-    var bsShift = 4;
-    var bsMask = 7;
-    var bsMap = {
-	4: 0x10000,
-	5: 0x40000,
-	6: 0x100000,
-	7: 0x400000
-    };
-
-    // Utility functions/primitives
-    // --
 
     function sliceArray (array, start, end) {
 	if (typeof array.buffer !== undefined) {
@@ -145,74 +53,6 @@ function LZ4() {
 	} else {
 	    // Assume normal array.
 	    return array.slice(start, end);
-	}
-    }
-
-    // Implementation
-    // --
-
-    // Calculates an upper bound for lz4 decompression, by reading the data.
-    this.decompressBound = function decompressBound (src) {
-	var sIndex = 0;
-
-	// Read magic number
-	if (util.readU32(src, sIndex) !== magicNum) {
-	    throw new Error('invalid magic number');
-	}
-
-	sIndex += 4;
-
-	// Read descriptor
-	var descriptor = src[sIndex++];
-
-	// Check version
-	if ((descriptor & fdVersionMask) !== fdVersion) {
-	    throw new Error('incompatible descriptor version ' + (descriptor & fdVersionMask));
-	}
-
-	// Read flags
-	var useBlockSum = (descriptor & fdBlockChksum) !== 0;
-	var useContentSize = (descriptor & fdContentSize) !== 0;
-
-	// Read block size
-	var bsIdx = (src[sIndex++] >> bsShift) & bsMask;
-
-	if (bsMap[bsIdx] === undefined) {
-	    throw new Error('invalid block size ' + bsIdx);
-	}
-
-	var maxBlockSize = bsMap[bsIdx];
-
-	// Get content size
-	if (useContentSize) {
-	    return util.readU64(src, sIndex);
-	}
-
-	// Checksum
-	sIndex++;
-
-	// Read blocks.
-	var maxSize = 0;
-	while (true) {
-	    var blockSize = util.readU32(src, sIndex);
-	    sIndex += 4;
-
-	    if (blockSize & bsUncompressed) {
-		blockSize &= ~bsUncompressed;
-		maxSize += blockSize;
-	    } else if (blockSize > 0) {
-		maxSize += maxBlockSize;
-	    }
-
-	    if (blockSize === 0) {
-		return maxSize;
-	    }
-
-	    if (useBlockSum) {
-		sIndex += 4;
-	    }
-
-	    sIndex += blockSize;
 	}
     }
 
@@ -290,108 +130,24 @@ function LZ4() {
 	return dIndex;
     };
 
-    // Decompresses a frame of Lz4 data.
-    this.decompressFrame = function decompressFrame (src, dst) {
-	var useBlockSum, useContentSum, useContentSize, descriptor;
-	var sIndex = 0;
-	var dIndex = 0;
+    this.decompress = function decompress(src, dst) {
+	let maxSize = src[0] << 8 | src[1];
 
-	// Read magic number
-	if (util.readU32(src, sIndex) !== magicNum) {
-	    throw new Error('invalid magic number');
-	}
+	console.log("len = " + maxSize);
 
-	sIndex += 4;
+	dst = this.makeBuffer(maxSize);
 
-	// Read descriptor
-	descriptor = src[sIndex++];
+	size = this.decompressBlock(src, dst, 2, maxSize, 0);
 
-	// Check version
-	if ((descriptor & fdVersionMask) !== fdVersion) {
-	    throw new Error('incompatible descriptor version');
-	}
-
-	// Read flags
-	useBlockSum = (descriptor & fdBlockChksum) !== 0;
-	useContentSum = (descriptor & fdContentChksum) !== 0;
-	useContentSize = (descriptor & fdContentSize) !== 0;
-
-	// Read block size
-	var bsIdx = (src[sIndex++] >> bsShift) & bsMask;
-
-	if (bsMap[bsIdx] === undefined) {
-	    throw new Error('invalid block size');
-	}
-
-	if (useContentSize) {
-	    // TODO: read content size
-	    sIndex += 8;
-	}
-
-	sIndex++;
-
-	// Read blocks.
-	while (true) {
-	    var compSize;
-
-	    compSize = util.readU32(src, sIndex);
-	    sIndex += 4;
-
-	    if (compSize === 0) {
-		break;
-	    }
-
-	    if (useBlockSum) {
-		// TODO: read block checksum
-		sIndex += 4;
-	    }
-
-	    // Check if block is compressed
-	    if ((compSize & bsUncompressed) !== 0) {
-		// Mask off the 'uncompressed' bit
-		compSize &= ~bsUncompressed;
-
-		// Copy uncompressed data into destination buffer.
-		for (var j = 0; j < compSize; j++) {
-		    dst[dIndex++] = src[sIndex++];
-		}
-	    } else {
-		// Decompress into blockBuf
-		dIndex = exports.decompressBlock(src, dst, sIndex, compSize, dIndex);
-		sIndex += compSize;
-	    }
-	}
-
-	if (useContentSum) {
-	    // TODO: read content checksum
-	    sIndex += 4;
-	}
-
-	return dIndex;
-    };
-
-    // Decompresses a buffer containing an Lz4 frame. maxSize is optional; if not
-    // provided, a maximum size will be determined by examining the data. The
-    // buffer returned will always be perfectly-sized.
-    this.decompress = function decompress (src, maxSize) {
-	var dst, size;
-
-	if (maxSize === undefined) {
-	    maxSize = this.decompressBound(src);
-	}
-	dst = util.makeBuffer(maxSize);
-	size = this.decompressFrame(src, dst);
+	console.log("decoded size = " + size);
 
 	if (size !== maxSize) {
 	    dst = sliceArray(dst, 0, size);
 	}
 
 	return dst;
-    };
+    }
 }
-
-/*
-var lz4 = new LZ4();
 
 function byteArray (arg) {
   if (Uint8Array) {
@@ -405,22 +161,18 @@ function byteArray (arg) {
   }
 }
 
-var output = byteArray([
-        0x54, 0x68, 0x65, 0x20, 0x77, 0x68, 0x6f, 0x6c,
-        0x65, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x20,
-        0x69, 0x73, 0x20, 0x65, 0x6e, 0x64, 0x69, 0x6e,
-        0x67, 0x2e, 0x0a
+var input = byteArray([
+0x00, 0x50,
+0xf1, 0x0d, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 
+0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x20, 0x68, 
+0x61, 0x68, 0x61, 0x2c, 0x20, 0x73, 0x6f, 0x6d, 
+0x65, 0x20, 0x6c, 0x69, 0x6b, 0x65, 0x0a, 0x00, 
+0x10, 0x21, 0x01, 0x00, 0x2b, 0x20, 0x41, 0x01, 
+0x00, 0xb2, 0x21, 0x20, 0x2d, 0x2d, 0x2d, 0x3d, 
+0x3d, 0x3d, 0x20, 0x48, 0x41, 0x02, 0x00, 0x80, 
+0x20, 0x3d, 0x3d, 0x3d, 0x2d, 0x2d, 0x2d, 0x00, 
 ]);
 
-var input = byteArray([
-        0x04, 0x22, 0x4d, 0x18, 0x64, 0x40, 0xa7, 0x1b,
-        0x00, 0x00, 0x80, 0x54, 0x68, 0x65, 0x20, 0x77,
-        0x68, 0x6f, 0x6c, 0x65, 0x20, 0x77, 0x6f, 0x72,
-        0x6c, 0x64, 0x20, 0x69, 0x73, 0x20, 0x65, 0x6e,
-        0x64, 0x69, 0x6e, 0x67, 0x2e, 0x0a, 0x00, 0x00,
-        0x00, 0x00, 0xbc, 0xa8, 0x6b, 0xc5
-]);
+var lz4 = new LZ4();
 
 console.log(lz4.decompress(input));
-console.log(output);
- */
