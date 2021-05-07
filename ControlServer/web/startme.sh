b701f0bc3e03e99311315f247e48f7c10074bfc7
pdaxrom <sashz@pdaxrom.org>
2021-05-07 11:11:49 +0400

    remove obsolete components

--- ControlServer/web/startme.sh
+++ ControlServer/web/startme.sh
@@ -10,16 +10,11 @@ if [ -e /cert/cert.pem -a -e /cert/privkey.pem ]; then
 
     sed -i -e "s|getenv('HOST_NAME')|'$HOST_NAME'|g" /var/www/html/desktop.php
 
-    mkdir -p /var/cache/token
-    chown www-data:www-data /var/cache/token
-
     mkdir -p /var/run/lighttpd
     chown www-data:www-data /var/run/lighttpd
 
     mkdir -p /var/lib/controlserver
     chown www-data:www-data /var/lib/controlserver
-
-    /usr/share/websockify/run -D --token-plugin TokenFile --token-source /var/cache/token 6080
 
     lighttpd -f /etc/lighttpd/lighttpd.conf
 
